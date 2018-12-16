b2fd403dddc3d38b1a512aa976fba23221c56278
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-12-16 17:16:29 -0500

    Updated Map.java
     - Added methods to add continents onto the map

--- src/map/Map.java
+++ src/map/Map.java
@@ -33,14 +33,57 @@ public class Map {
 		int climate = (int) Math.random() * 3;
 		
 		// Random variable for age - mountain ratio
+		int age = (int) Math.random() * 3;
+		
+		// This variable determines how large a perfect continent would be
+		int pCSize = (int) (((N * N) * 0.3) / numContinents);
 		
 		// This loop randomly generates numContinents continents
 		for (int cont=0; cont<numContinents; cont++) {
+			// All numContinents continents are proportional to a third of the size of the total size (N * N) / numContinents
+			
+			// Get the size of the continent (with noise)
+			int cSize = addNoiseToContSize(pCSize);
+			
+			// Randomly initialize continent center position
+			int x = (int) Math.random() * N;
+			int y = (int) Math.random() * N;
+			
+			addContinentToMap(x, y, cSize);
 			
 		}
 		
-		// Default tile is grassland on land and ocean on water
+		// Default tile is grassland for land based tiles and ocean for water based tile
 		
+	}
+	
+	// This method adds a continent (all grassland) to the map
+	private void addContinentToMap (int x, int y, int size) {
+		// Get the approximate 
+		int rad = (int) Math.sqrt(size / Math.PI);
+		
+		grid[x][y] = new Grassland(null, null, null);
+		
+		// Top left corner
+		int x1 = x - rad;
+		int y1 = y - rad;
+		// Bottom Right corner
+		int x2 = x + rad;
+		int y2 = y + rad;
+		
+		for (int r=x1; r<x2; r++) {
+			for (int c=y1; c<y2; c++) {
+				grid[r][c] = new Grassland(null, null, null);
+			}
+		}
+		
+	}
+	
+	// This method adds noise to a perfect sized continent
+	private int addNoiseToContSize (int pCSize) {
+		// This variable determines the variance (-25% to +25%)
+		double ranProportion = Math.random() - 0.75;
+		return (int) (pCSize * ranProportion);
 	}
 
 }
